728c2002944617bf14fa7ddd442503d70152c867
Binhg <binhuguang@foxmail.com>
2019-05-20 17:35:45 +0800

    新增book相关api

---
+++ src/config/api/book.api.js
@@ -0,0 +1,16 @@
+import { BaseServerPath } from './basePath';
+
+
+export const BOOK_GOOD_LIST = `${BaseServerPath}/book/queryGoodList`
+
+export const BOOK_TRASH_LIST = `${BaseServerPath}/book/queryGarbageList`
+
+export const BOOK_SEARCH = `${BaseServerPath}/book/search`
+
+export const BOOK_ADD = `${BaseServerPath}/book/add`
+
+export const BOOK_DROP = `${BaseServerPath}/book/drop2Garbage`
+
+export const BOOK_PICK = `${BaseServerPath}/book/pickFromGarbage`
+
+export const BOOK_ADD_BOOK_MARK = `${BaseServerPath}/book/addBookMark`
