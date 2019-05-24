abf09d3cd6a499af7f2078604079f2fe43c2edbb
Binhg <binhuguang@foxmail.com>
2019-05-24 18:26:32 +0800

    ..

--- src/container/book/Good.js
+++ src/container/book/Good.js
@@ -3,62 +3,50 @@ import { Text, View } from 'react-native';
 import { postUrl } from 'wl-fetch';
 import { r } from '../../utils/layout';
 import { IconFill } from '@ant-design/icons-react-native';
-import {
-    createStackNavigator,
-    createAppContainer
-} from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 
-class GoodScreen extends Component{
+class GoodScreen extends Component {
+	static navigationOptions = (props) => ({
+		title: '测试头'
+	});
 
-	// static navigationOptions = props=>({
-	// 	title:'测试头'
-	// })
-
-	render(){
+	render() {
 		return (
 			<View>
-				<Text>
-					GoodList
-				</Text>
+				<Text>GoodList</Text>
 			</View>
-		)
+		);
 	}
 }
 
-export const GoodNavigator = createStackNavigator({
-	GoodScreen:{
-		screen:GoodScreen,
-		navigationOptions:{
-			title:'测试头'
-		}
+export const GoodNavigator = createStackNavigator(
+	{
+		GoodScreen: GoodScreen
+	},
+	{
+		initialRouteName: 'GoodScreen'
 	}
-},{
-	initialRouteName:'GoodScreen'
-});
+);
 
 export default class Good extends Component {
-	
-	static navigationOptions = props=>({
-		title:'追更',
-		tabBarIcon:({focused,tintColor})=>(
+	static navigationOptions = (props) => ({
+		title: '追更',
+		tabBarIcon: ({ focused, tintColor }) => (
 			<IconFill
-			  name="book"
-			  size={r(30)}
-			  color={tintColor} 
+				name="book" 
+				size={r(30)}
+				color={tintColor}
 			/>
 		)
-	})
+		
+	});
 
-    componentDidMount(){
-        // postUrl('')
-    }
+	componentDidMount() {
+		// postUrl('')
+	}
 
-    render() {
-		return (
-			<View>
-				{/* <GoodContainer/> */}
-			</View>
-		);
+	render() {
+		return <View>{/* <GoodContainer/> */}</View>;
 	}
 }
